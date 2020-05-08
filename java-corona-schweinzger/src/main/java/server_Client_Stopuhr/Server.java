a583cc7f210126d06de8a46b0ecb69e4aa6ba93e
swemam16 <swemam16@htl-kaindorf.ac.at>
2020-05-08 10:43:13 +0200

    aktualisiert 08.05.20

--- java-corona-schweinzger/src/main/java/server_Client_Stopuhr/Server.java
+++ java-corona-schweinzger/src/main/java/server_Client_Stopuhr/Server.java
@@ -5,6 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketImpl;
@@ -23,89 +24,115 @@ public class Server {
     
     public void start (int port) throws IOException {
         serverSocket = new ServerSocket(port);
-        
-        timeOffset = 0;
         startMillis = System.currentTimeMillis();
         
         while(true){
             final Socket clientSocket = serverSocket.accept();
-            final ConnectionHandler handler = new ConnectionHandler(clientSocket);    
-            new Thread(handler).start();
-            handlers.add(handler);
+            for(ConnectionHandler h: handlers){
+                    if(h.isClosed()){
+                        handlers.remove(h);
+                    }
+            }
+            if(handlers.size() == 3){
+                clientSocket.close();
+                continue;
+            }
+                final ConnectionHandler handler = new ConnectionHandler(clientSocket);    
+                new Thread(handler).start();
+                handlers.add(handler);
         }   
     }
     
     public boolean isTimerRunning() {
-        return         
+        return startMillis > 0;
     }
     
     public long getTimerMillis() {
-        return  System.currentTimeMillis() - startMillis;
+        if(startMillis > 0){
+            return  System.currentTimeMillis() - startMillis + timeOffset;
+        } else {
+            return timeOffset;
+        }
     }
     
-    public static void main(String[] args){
-        new Server();
+    public static void main(String[] args) throws IOException{
+        new Server().start(8080);
     }
-
 
 
     class ConnectionHandler implements Runnable {
 
-        private Socket socket;
+        private final Socket socket;
         private boolean master;
 
         public ConnectionHandler(Socket socket) {
-
+            this.socket = socket;
         }
 
         public boolean isClosed() {
-            return         
+            return socket.isClosed();    
         }
 
         public boolean isMaster() {
-            return 
+            return master;
         }
 
         @Override
         public void run() {
-            try {
-                BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String line;
-                line = reader.readLine();
+            int cnt = 0;
 
-                Gson gson = new Gson();
-                gson.toJson(line);
-                Request req = gson.fromJson(line, Request.class);
-                
-                if(req.isMaster()){
-                    for(ConnectionHandler c : handlers){
-                        this.master = true;
-                        if(c != this && c.isMaster() == true){
-                            this.master = false;
+            while(true){
+                try {
+                    final BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    final String requestLine = reader.readLine();
+                    cnt++;
+                    final Gson gson = new Gson();
+                    gson.toJson(requestLine);
+                    Request req = gson.fromJson(requestLine, Request.class);
+
+                    if(req.isMaster()){
+                        for(ConnectionHandler c : handlers){
+                            this.master = true;
+                            if(c != this && c.isMaster() == true){
+                                this.master = false;
+                            }
+                        }
+                    }    
+                    
+                    if(req.isMaster()){
+                        if(req.isStart()){
+                            startMillis = System.currentTimeMillis();
+                        }
+                        if(req.isStop()){
+                            startMillis = 0;
+                        } else {
+                            timeOffset = getTimerMillis();
+                        }
+                        if(req.isClear()){
+                            timeOffset = 0;
+                            if(isTimerRunning()){
+                                startMillis = System.currentTimeMillis();
+                            } else {
+                                startMillis = 0;
+                            }
+                        }
+                        if(req.isEnd()){
+                            serverSocket.close();
+                            socket.close();
+                            handlers.remove(this);
+                            return;
                         }
                     }
-                }        
-                if(req.isStart()){
-                    startMillis = System.currentTimeMillis();
+
+                    Response resp = new Response(master, cnt, isTimerRunning(), getTimerMillis());
+                    String responseLine = gson.toJson(resp);
+                    final OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream());
+                    writer.write(responseLine);
+                    writer.flush();
+                    
+                } catch (Exception ex) {
+                    ex.printStackTrace();
                 }
-                if(req.isStop()){
-                    startMillis = 0;
-                } else {
-                    timeOffset = getTimerMillis();
-                }
-                if(req.isClear()){
-                    timeOffset = 0;
-                }
-                if(req.isEnd()){
-                    socket.close();
-                    handlers.remove(this);
-                }
-                
-                
- 
-            
-            } catch (Exception ex) {
-                ex.printStackTrace();
             }
         }   
     }
